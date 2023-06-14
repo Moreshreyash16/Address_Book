1a3851e56ca654d89862433f3c616d19de5322f8
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-14 15:23:56 +0530

    [Shreyash] Add : UC1 To create Address book

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-14 15:11:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-14 15:11:30
+
+@Title : Address book uc 1 to create a addressbook
+
+'''
+from addressbook import AddressBook
+from contact import Contact
+
+def main():
+    # creating the addressbook
+    address_book=AddressBook()          
+if __name__ == "__main__":
+    main()
