8068df2389612f4be561f8fe9102c6daa8a6cff8
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-14 20:28:59 +0530

    [Shreyash] Add : UC6 it is creates all the log file

---
+++ log.py
@@ -0,0 +1,10 @@
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+handlers=[
+        logging.StreamHandler(),  # Stream logs to the console
+        logging.FileHandler('address,log')  # Store logs in a file
+    ])
+
+
+    # Create a logger
+logger = logging.getLogger('my_logger')
