c9a14ecc459421f45427fe543284f7037b882a6e
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-06-15 19:40:19 +0530

    UC3 it stores method for addressbook

--- addressbook.py
+++ addressbook.py
@@ -57,8 +57,7 @@ class AddressBook:
         """
         index = self.contacts.index(contact)
         return index
-    
-    
+     
     def update_firstname(self,new_firstname,index):
         """
         Description:
